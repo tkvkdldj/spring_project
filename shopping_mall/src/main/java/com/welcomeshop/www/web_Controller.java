19a50cb24a4526dd0a4e5ae1332f1022c5ac69f3
tkvkdldj <dlgmldn00@gmail.com>
2024-07-29 01:19:37 +0900

    server setting success

---
+++ shopping_mall/src/main/java/com/welcomeshop/www/web_Controller.java
@@ -0,0 +1,10 @@
+package com.welcomeshop.www;
+
+import java.io.PrintWriter;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class web_Controller {
+	PrintWriter pw = null;
+}
