55fd3fe3c55533a8f9ea68c293cf2914dba5e133
tkvkdldj <dlgmldn00@gmail.com>
2024-07-29 01:28:46 +0900

    web_Controller

--- shopping_mall/src/main/java/com/welcomeshop/www/web_Controller.java
+++ shopping_mall/src/main/java/com/welcomeshop/www/web_Controller.java
@@ -3,8 +3,13 @@ package com.welcomeshop.www;
 import java.io.PrintWriter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_Controller {
 	PrintWriter pw = null;
+	@GetMapping("/ajaxok.do")
+	public String ajaxok() {
+		return null;
+	}
 }
